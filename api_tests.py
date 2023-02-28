fe4977cf9ad26c59051329324974ffcbef8ca71f
Matt Marchio <mattmarchio1@gmail.com>
2023-02-27 20:58:43 -0500

    update api classes

---
+++ api_tests.py
@@ -0,0 +1,12 @@
+from api.oanda_api import OandaAPI
+from infrastructure.instrument_collection import instrumentCollection
+import time
+
+if __name__ == '__main__':
+    api = OandaAPI()
+    instrumentCollection.loadInstruments("./data")
+    trade_id = api.place_trade("EUR_USD", 100, 1)
+    print("Opened:", trade_id)
+
+    time.sleep(10)
+    print(f"Closing {trade_id}", api.close_trade(trade_id))
